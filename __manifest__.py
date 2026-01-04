af4fe1bef729e068896e851b5f687635c4b4197d
Adam Blunt <adamxp12@adamxp12.com>
2026-01-04 21:52:45 +0000

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'POS Epson Direct XML Printing',
+    'version': '19.0.1.0',
+    "author": "ABSecure",
+    "website": "https://absecure.uk/",
+    'category': 'Point of Sale',
+    'summary': 'Send raw XML commands to Epson printers for preparation orders.',
+    'depends': ['point_of_sale'],
+    'assets': {
+        'point_of_sale._assets_pos': [
+            'pos_epson_xml_direct/static/src/js/epson.js'
+        ],
+    },
+    'installable': True,
+    'license': 'LGPL-3',
+    'price': 0.00
+}
